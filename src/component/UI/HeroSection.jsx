292e6a84885da2739b612bd1baa5a9cab961573b
Rohit Harne <harnerohit832@gmail.com>
2025-10-30 11:35:54 +0530

    Added NavLink to Start Exploring button in HeroSection

--- src/component/UI/HeroSection.jsx
+++ src/component/UI/HeroSection.jsx
@@ -1,31 +1,37 @@
 import React from 'react'
-import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md"
+import { NavLink } from 'react-router-dom'
 
 const HeroSection = () => {
   return (
-    <div>
-      <>
-      <main className='hero-section main'>
-        <div className='container grid grid-two-cols'>
-          <div className='hero-content'>
-            <h1 className='heading-xl'>
-              Explore the World with WorldAtlas
-            </h1>
-            <p className='paragraph'>
-              Discover the history. culture, and beauty of every nation. Sort. search. and filter through countries to find the details you need.
-            </p>
+    <main className='hero-section main'>
+      <div className='container grid grid-two-cols'>
+        <div className='hero-content'>
+          <h1 className='heading-xl'>
+            Explore the World with WorldAtlas
+          </h1>
+
+          <p className='paragraph'>
+            Discover the history, culture, and beauty of every nation. 
+            Sort, search, and filter through countries to find the details you need.
+          </p>
+
+          <NavLink to="/about">
             <button className='btn btn-darken btn-inline bg-white-box'>
               Start Exploring <MdKeyboardDoubleArrowRight />
-
             </button>
-          </div>
-          <div className='hero-image'>
-            <img src="/images/world.png" alt="world is beauty" className='banner-image' />
-          </div>
+          </NavLink>
         </div>
-      </main>
-      </>
-    </div>
+
+        <div className='hero-image'>
+          <img 
+            src="/images/world.png" 
+            alt="world is beauty" 
+            className='banner-image' 
+          />
+        </div>
+      </div>
+    </main>
   )
 }
 
